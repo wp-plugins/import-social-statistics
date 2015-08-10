96f61e9925a5fdc450f939f9459523d9f9102267
easantos <easantos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-10 22:32:30 +0000

    Add tag 1.0.0 history
    git-svn-id: https://plugins.svn.wordpress.org/import-social-statistics/tags/1.0.0@1217659 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,40 @@
+=== Import Social Statistics ===
+Contributors: easantos
+Donate link: http://www.easantos.net/
+Tags: facebook, likes, number of likes
+Requires at least: 4.2.2
+Tested up to: 4.2.2
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Import Social Statistics tracks the number of social interactions for each post.
+
+== Description ==
+
+Import Social Statistics tracks the number of social interactions for each post. Sorts your posts by the number of Facebook likes and finds you most shared content.
+
+== Installation ==
+
+1. Upload `import-social-statistics` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= How frequent is the counter updated? =
+
+The counter is updated each time a post is viewed. If a cache plugin is in use then the counter will be updated when the cache expires.
+
+== Screenshots ==
+
+1. Number of Facebook likes in post listings
+
+== Changelog ==
+
+= 1.0 =
+* First version of the plugin.
+
+== Upgrade Notice ==
+
+= 1.0 =
+First version of the plugin.
---
+++ views/main.php
@@ -0,0 +1,46 @@
+<div class="wrap columns-2 dd-wrap">
+    <div class="icon32" id="icon-options-general"></div>
+    <h2>Import Social Statistics</h2>
+
+    <div class="metabox-holder has-right-sidebar" id="poststuff">
+        <div class="metabox-holder has-right-sidebar" id="poststuff">
+
+            <div id="side-info-column" class="inner-sidebar">
+
+                <div class="postbox">
+                    <h3>About Import Social Statistics</h3>
+
+                    <div class="inside">
+                        <p>Name : Import Social Statistics</p>
+
+                        <p>Author : <a target="_blank" href="http://www.easantos.net">Easantos</a></p>
+
+                        <p>Website : <a href="http://www.easantos.net" target="_blank">www.easantos.net</a></p>
+
+                        <p>Email : <a href="mailto:suporte@easantos.net" target="_blank">suporte@easantos.net</a></p>
+                        <center><img src="http://www.easantos.net/logo.png"></center>
+                        <p><strong>Easantos</strong> provides a full range of WordPress web development services, including
+                            theme implementation and plugin development at competitive prices.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div id="post-body">
+            <div id="post-body-content">
+                <div class="stuffbox">
+                    <h3><label for="link_name">How to Use Import Social Statistics</label></h3>
+
+                    <div class="inside">
+                        <p>As soon as you enable the Import Social Statistics plugin several new columns in the post listing page will
+                            appear and a counter for each tracked social network will be displayed.</p>
+
+                        <p>Over time all your posts will be tracked and you can sort your posts by the number of
+                            social interactions, like Facebook likes.</p>
+                        <p>Keep track of your most valuable content with this plugin and find your most shared content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
